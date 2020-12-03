0d090f8d32c33a2daa2e141f75ff4261ccc04266
Mathis Engelbart <mathis.engelbart@gmail.com>
2020-12-03 17:24:59 +0100

    Draft experiment detail view

---
+++ src/components/ExperimentDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="detail">
+    <LineChart class="ssim" :experiment="this.experiment" metric="SSIM"/>
+    <LineChart class="psnr" :experiment="this.experiment" metric="PSNR"/>
+    <LineChart class="scream-congestion" :experiment="this.experiment" metric="scream-congestion"/>
+    <LineChart class="scream-bitrate" :experiment="this.experiment" metric="scream-bitrate"/>
+    <LineChart class="scream-queue-length"
+      :experiment="this.experiment"
+      metric="scream-queue-length"/>
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart.vue';
+import Experiment from '@/experiment';
+
+export default {
+  name: 'ExperimentDetail',
+  components: { LineChart },
+  props: {
+    experiment: Experiment,
+  },
+};
+</script>
+
+<style scoped>
+.detail {
+  display: grid;
+}
+.ssim {
+  grid-column: 1;
+}
+.psnr {
+  grid-column: 1;
+}
+.ssim-cdf {
+  grid-column: 2;
+}
+.psnr-cdf {
+  grid-column: 2;
+}
+</style>
